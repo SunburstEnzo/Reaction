c2bd3e5f36752b25b973930fcee328f5e6172d2b
Aled Samuel <samuelaled@gmail.com>
2016-12-12 16:22:40 +0000

    Initial Commit

---
+++ Reaction/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Reaction
+//
+//  Created by Aled Samuel on 12/12/2016.
+//  Copyright © 2016 Aled Samuel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Reaction/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reaction
+//
+//  Created by Aled Samuel on 12/12/2016.
+//  Copyright © 2016 Aled Samuel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
